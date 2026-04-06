ce8635e52846c374cdf02a8a922a2f6ebb1dd7f8
ivanhromovyi <ivanh@uia.no>
2026-04-06 14:34:37 +0200

    Add GlobalExcpetionHandler

---
+++ src/main/java/ihromovyi/tacocloud/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package ihromovyi.tacocloud.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST);
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(this::getErrorMessage)
+                .toList();
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler({
+            IngredientNotFoundException.class,
+            TacoNotFoundException.class,
+            TacoOrderNotFoundException.class,
+            EntityNotFoundException.class,
+            IllegalArgumentException.class
+    })
+    public ResponseEntity<Object> handleRuntimeException(
+            RuntimeException e
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
+    private String getErrorMessage(ObjectError objectError) {
+        if (objectError instanceof FieldError) {
+            String field = ((FieldError) objectError).getField();
+            String message = objectError.getDefaultMessage();
+            return field + " " + message;
+        }
+        return objectError.getDefaultMessage();
+    }
+}
+
